a6b31b96639c014e4f29d8421b28efd05fa6e26a
Surya <suryasharmaiosdev@gmail.com>
2024-12-19 18:28:33 +0530

    updating demo site

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'i2zadmin-dashboard-starter-v2';
+  title = 'i2zadmin-dashboard-starter';
   sidebarExpanded = true;
   currentSidebarMode: any ='mini';
   mainNavigationOpened = true;
